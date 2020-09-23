9aa69d95f1f2c1285a49148907e147dd89a33314
sathish kumar <rbsathish27@gmail.com>
2020-09-23 20:25:13 +0530

    finding same name files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # find_matching_files
- find the same name files
+
+        Find the same file name present are not if present means copying that files to matching_files folder 
+        This will be helpful for dataset preparation in deep learning or you can just use for other purpose also.
+
+
+matcher.py
+
+        In matcher.py file first you need to give the folder paths where you want to find matching files.
+
+        Here i used to find the matching names for xml files in one folder and png images in other folder.
+        (you can change png to jpg if your images is jpg means)
+
+        If same name matches in both folder means i just copy the matched xml and png files to matching_files.
+        (if you want to copy the png only means you can comment the xml saving part or if you want copy the xml means you can comment the png part) 
---
+++ matcher.py
@@ -0,0 +1,63 @@
+import glob
+import shutil, os
+
+cwd = os.getcwd()
+'''
+folder path :
+	xml path
+	image path
+	create matching_files folder
+file format :
+	In this example i used png if you are having jpg formats means you can change all png to jpg and use
+'''
+
+cwd_xml 	= cwd + "\\" + "xml\\"
+cwd_frames  = cwd + '\\' + 'test\\'
+pathC = cwd + '\\matching_files\\'
+len_xml = len(cwd_xml)
+len_frames = len(cwd_frames)
+a1 = '.png'
+count = 1
+
+files_xml = [f for f in glob.glob(cwd_xml + "**/*.xml", recursive=True)]
+files_frames = [f for f in glob.glob(cwd_frames + "**/*.png", recursive=True)]
+
+'''
+find the file names with same name and copy the matched filels to matching_files 
+'''
+# copying png 
+# copy the same xml name png files to matching_files folder
+for f in files_xml:
+
+	f = f[len_xml:]
+	f = f[:-3]
+	f = f + 'png'
+	
+	print(count)
+	count += 1
+	for f1 in files_frames:
+		f1 = f1[len_frames:]
+		if f == f1 :
+			f1 = cwd_frames + '\\' + f1
+			shutil.copy(f1, pathC)
+
+# copying xml
+# copy the same png name xml files to matching_files folder			
+for f in files_frames:
+	f = f[len_frames:]
+	f = f.split(".")[0]
+	f = f+".xml"
+	
+	
+	for f1 in files_xml:
+		f1 = f1[len_xml:]
+		if f == f1:
+			f1 = cwd_xml + '\\' + f1
+			shutil.copy(f1, pathC)
+ 
+print("_________________________________________________________________________________")
+
+
+'''
+if you want to perform any one of this task means you can just command that and use.
+'''
